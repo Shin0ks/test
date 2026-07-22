65509b3dfaf8e77cb8ff115b8111685d6b2b22df
Shin0ks <sh1n0k@tutamail.com>
2026-07-22 12:41:13 -0300

    Update calculadora_imc.py

--- piloto_python/calculadora_imc.py
+++ piloto_python/calculadora_imc.py
@@ -7,7 +7,7 @@ altura =  float (input ('Qual a sua altura? '))
 #Calculadora 
 imc = peso / (altura * altura )
 
-#resultado
+#Resultado
 print (f'IMC: {imc:.2f}')
 
 #Definir imc 
@@ -21,3 +21,6 @@ elif 25<= imc <30:
     print ('gordo') 
 else: 
    print ('vai morrer')
+
+#Calculadora imc bem básica e simples, precisa apenas colocar os dados  e já vai sair o resultado. 
+#Lembrando que os padrões de imc são apenas estimativas de saúde, não levem a sério as classificações :). 
